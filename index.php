b2c1181cbdcdd757130ec31f671b873a34c46e18
Developer-Fan <0681691@schoolsnet.act.edu.au>
2022-06-09 20:27:47 +1000

    Create basic things

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php 
-
+$conn = new SQLite3("./db/database.sqlite");
+$sql = "
+CREATE TABLE IF NOT EXISTS usrs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    password TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+";
+$res = $conn->exec($sql);
+if (!$res) {
+    echo "Error creating table: " . $conn->lastErrorMsg();
+}
+$conn -> close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Synenergy Admin</title>
+    <link rel = "stylesheet" href = "styles/index.css" type = "text/css">
+</head>
+<body>
+    <div class = "menu">
+        <h3>Synenergy official administration website</h3>
+        <a id = "current">Home</a>
+        <a href = "login.php">Login</a>
+    </div>
+</body>
+</html>
